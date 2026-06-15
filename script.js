d9187096687da303610883049f896f9b264bcdd8
oumouyacouba <oumouyacouba1234@gmail.com>
2026-06-15 17:03:34 +0000

    get DOM element

---
+++ script.js
@@ -0,0 +1,4 @@
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+const btn = document.getElementById("btn")
+const result = document.getElementById("result")
