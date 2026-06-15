71f939fb6b9d2dea0eee544fb4328a058f8f9734
oumouyacouba <oumouyacouba1234@gmail.com>
2026-06-15 17:05:33 +0000

    add onclick function

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const username = document.getElementById("username")
 const password = document.getElementById("password")
 const btn = document.getElementById("btn")
 const result = document.getElementById("result")
+btn.onclick = function(){
+    
+}
