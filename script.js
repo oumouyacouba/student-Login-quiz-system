7e97c94cf78c1a0f995029821118c323eeed9515
oumouyacouba <oumouyacouba1234@gmail.com>
2026-06-16 20:40:26 +0000

    add condition using if and else

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ const password = document.getElementById("password")
 const btn = document.getElementById("btn")
 const result = document.getElementById("result")
 btn.onclick = function(){
+    const inputusername = username.value
+    const inputpassword = password.value
+     // Vérification des champs
+    if (inputusername === "" || inputpassword === "") {
+        result.textContent = "Please fill all fields";
+    } 
+    else {
+        result.textContent = "Registration successful";
+    }
     
+
 }
